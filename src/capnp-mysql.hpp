cb8fd4f605602c94b8e6ba3bcb3b2bd3f342fb8d
Seth Shelnutt <Shelnutt2@gmail.com>
2017-07-10 08:17:07 -0400

    Add capnp schema creation from mysql table defintiion. #1

---
+++ src/capnp-mysql.hpp
@@ -0,0 +1,30 @@
+//
+// Created by seth on 7/6/17.
+//
+
+#ifndef CAPNP_MYSQL_HPP
+#define CAPNP_MYSQL_HPP
+
+#include <string>
+#include <unordered_map>
+#include <map>
+
+#if !_MSC_VER
+#include <unistd.h>
+#endif
+#include <fcntl.h>
+
+#include <field.h>
+
+uint64_t generateRandomId();
+
+
+std::string buildCapnpSchema(std::map<u_int, std::unordered_map<std::string, std::string>> *columnMap,
+                             std::string structName, int *err, uint64_t id = 0);
+
+
+std::string buildCapnpLimitedSchema(Field **field,
+                             std::string structName, int *err, uint64_t id = 0);
+
+std::string getCapnpTypeFromField(Field *field);
+#endif //CAPNP_MYSQL_HPP
