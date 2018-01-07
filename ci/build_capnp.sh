09aa530c43b1ea6cecd4fd4f33accb1089bc297f
Seth Shelnutt <Shelnutt2@gmail.com>
2018-01-07 09:12:47 -0500

    Switch to ninja-build for travis ci. #46

--- ci/build_capnp.sh
+++ ci/build_capnp.sh
@@ -4,7 +4,7 @@ curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz &&
 tar zxf capnproto-c++-0.6.1.tar.gz &&
 cd capnproto-c++-0.6.1 &&
 ./configure &&
-make -j6 check &&
+make -j4 check &&
 sudo make install &&
 cd ../ &&
 sudo rm -rf capnproto-c++-0.6.1
