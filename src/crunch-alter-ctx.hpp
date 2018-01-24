809db12a35c985cb9d2dab2eef23acfd774cfa7e
Seth Shelnutt <Shelnutt2@gmail.com>
2018-01-23 22:28:58 -0500

    Add new crunchInplaceAlterCtx for inplace_alter api. #50

---
+++ src/crunch-alter-ctx.hpp
@@ -0,0 +1,39 @@
+/*
+** Licensed under the GNU Lesser General Public License v3 or later
+*/
+
+#ifndef CRUNCH_CRUNCH_ALTER_CTX_HPP
+#define CRUNCH_CRUNCH_ALTER_CTX_HPP
+
+#include <handler.h>
+#include <table.h>
+#include <cstdint>
+
+
+/**
+ * @breif In-place alter handler context.
+ */
+class crunchInplaceAlterCtx : public inplace_alter_handler_ctx {
+
+public:
+    crunchInplaceAlterCtx(std::string baseDirectory, std::string transactionDirectory, TABLE *alteredTable,
+                          uint64_t schemaVersion);
+
+    ~crunchInplaceAlterCtx() {}
+
+    bool buildNewCapnpSchema();
+
+    bool commit();
+
+    bool rollback();
+
+private:
+    std::string baseDirectory;
+    std::string transactionDirectory;
+    TABLE *alteredTable;
+    uint64_t schemaVersion;
+    std::string schemaFileName;
+    std::string transactionSchemaFile;
+};
+
+#endif // CRUNCH_CRUNCH_ALTER_CTX_HPP
