0cfe9064d00caf829d4d3bbeb3ce019a92e85d31
Seth Shelnutt <Shelnutt2@gmail.com>
2018-03-26 07:18:23 -0400

    Store index schema files with their index id. #57

--- src/crunch-index.cpp
+++ src/crunch-index.cpp
@@ -7,6 +7,12 @@
 #include "utils.hpp"
 #include "capnp-mysql.hpp"
 
+/**
+ * @brief Function to read all indexes and create schema files
+ *
+ * @param table_arg to read keys from
+ * @return status code
+ */
 int crunch::createIndexesFromTable(TABLE *table_arg) {
   DBUG_ENTER("crunch::createIndexFromTable");
   int rc = 0;
@@ -14,16 +20,17 @@ int crunch::createIndexesFromTable(TABLE *table_arg) {
 
   for(uint i = 0; i < table_arg->s->keys; i++) {
     KEY key = table_arg->key_info[i];
+    // Build capnp schema for given key
     std::string indexSchema = buildCapnpIndexSchema(&key, "", &rc, 0);
     File create_file;
-    // Let mysql create the file for us
+    // Create index schema file
     if ((create_file = my_create(fn_format(name_buff, (baseFilePath + "-" + key.name).c_str(), "",
-                                           TABLE_INDEX_SCHEME_EXTENSION,
+                                           (std::to_string(i) + "." + TABLE_INDEX_SCHEME_EXTENSION).c_str(),
                                            MY_REPLACE_EXT | MY_UNPACK_FILENAME), 0,
                                  O_RDWR | O_TRUNC, MYF(MY_WME))) < 0)
       DBUG_RETURN(-1);
 
-    // Write the capnp schema to schema file
+    // Write the capnp index schema to file
     ::write(create_file, indexSchema.c_str(), indexSchema.length());
     my_close(create_file, MYF(0));
   }
